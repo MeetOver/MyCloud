357bb7e7624ebf6129fd82138a98b80715980e25
MeetOver <kastor10.1@o2.pl>
2023-10-13 12:17:48 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<head>
+    <title>Pisarek</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head> 
+Zadanie 1. Logowanie, Sesja, Rejestracja
+<br />
+<a href ="rejestruj.php">Program logowania do komunikatora</a><br />
+
+<br/>
+<a href ="http://mateuszpisarek.free.nf">Powrót</a>
+<br />
